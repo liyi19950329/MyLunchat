7cb8a7d3be637b6a9cf6bc3a1647d75ea48a2ef5
duebydue <2996460970@qq.com>
2019-10-11 21:55:18 +1100

    beifen

--- Lunchat/Controller/ManageViewController.swift
+++ Lunchat/Controller/ManageViewController.swift
@@ -1,11 +1,19 @@
+//
+//  ManageViewController.swift
+//  Lunchat
+//
+//  Created by JamesCullen on 2019/9/11.
+//  Copyright © 2019 MobileTeam. All rights reserved.
+//
+
 import UIKit
 
 class ManageViewController: UIViewController {
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        print("mmp")
     }
     
-    
+
 }
